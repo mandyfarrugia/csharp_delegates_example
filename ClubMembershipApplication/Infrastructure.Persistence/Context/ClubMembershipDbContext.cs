6afcd78833de6a647a575adfb2f2e67e895878d4
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-02-04 17:54:38 +0100

    Setting up an SQLite database provider using a connection string pointing to a .db file within the directory where assemblies reside in the Infrastructure.Persistence library.

---
+++ ClubMembershipApplication/Infrastructure.Persistence/Context/ClubMembershipDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Context
+{
+    public class ClubMembershipDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"{AppDomain.CurrentDomain.BaseDirectory}ClubMembershipDb.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
