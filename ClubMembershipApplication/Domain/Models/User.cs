c55c2b1de3a0ca0ac5a3bc1f4914d43e5e9fb12e
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-02-04 17:48:33 +0100

    Installed EntityFrameworkCore and Microsoft.EntityFrameworkCore.Sqlite to include an SQLite database provider for Entity Framework Core.

---
+++ ClubMembershipApplication/Domain/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AddressFirstLine { get; set; }
+        public string AddressSecondLine { get; set; }
+        public string PostCode { get; set; }
+    }
+}
