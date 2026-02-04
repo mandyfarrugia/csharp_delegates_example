356ff4bba08cefb72da8be4cca7ba18b90acb32a
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-02-04 17:15:15 +0100

    Presentation will comprise of different templates targeting a specific platform. For the sake of this example, a console application will be used as the focal point is persistence and delegates in C#.

---
+++ ClubMembershipApplication/Presentation.UI.ConsoleApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentation.UI.ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
