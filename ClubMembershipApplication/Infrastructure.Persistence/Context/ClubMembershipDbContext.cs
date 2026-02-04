ed6662c225a87fd2cbacb2a547ecfd99b0a2bf57
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-02-04 18:14:05 +0100

    Removed constructor from DbContext class to prevent the error: Unable to create a 'DbContext' of type 'ClubMembershipDbContext'. The exception 'Unable to resolve service for type 'Microsoft.EntityFrameworkCore.DbContextOptions`1[Infrastructure.Persistence.Context.ClubMembershipDbContext]' while attempting to activate 'Infrastructure.Persistence.Context.ClubMembershipDbContext'.' was thrown while attempting to create an instance. For the different patterns supported at design time, see https://go.microsoft.com/fwlink/?linkid=851728

--- ClubMembershipApplication/Infrastructure.Persistence/Context/ClubMembershipDbContext.cs
+++ ClubMembershipApplication/Infrastructure.Persistence/Context/ClubMembershipDbContext.cs
@@ -5,8 +5,6 @@ namespace Infrastructure.Persistence.Context
 {
     public class ClubMembershipDbContext : DbContext
     {
-        public ClubMembershipDbContext(DbContextOptions<ClubMembershipDbContext> options) : base(options) {}
-
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
