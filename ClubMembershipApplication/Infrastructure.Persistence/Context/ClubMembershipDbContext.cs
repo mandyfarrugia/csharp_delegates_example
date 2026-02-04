5c5b3be78cafeb9735306af7c4dc918eab3d0079
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-02-04 18:01:09 +0100

    Defined a property to access the Users table from the database context instance.

--- ClubMembershipApplication/Infrastructure.Persistence/Context/ClubMembershipDbContext.cs
+++ ClubMembershipApplication/Infrastructure.Persistence/Context/ClubMembershipDbContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence.Context
 {
     public class ClubMembershipDbContext : DbContext
     {
+        public ClubMembershipDbContext(DbContextOptions<ClubMembershipDbContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite($"Data Source={AppDomain.CurrentDomain.BaseDirectory}ClubMembershipDb.db");
